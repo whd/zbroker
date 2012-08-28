c7310609ab8bb5c459a2d3920c4ebcf9afe7ad9c
Wesley Dawson <wdawson@mozilla.com>
2012-08-28 12:18:49 -0700

    Typo

--- lib/zbroker/lcs.rb
+++ lib/zbroker/lcs.rb
@@ -1,5 +1,5 @@
 module ZBroker::LCS
-  # implementation of longest common substring algorithm from
+  # implementation of a longest common substring algorithm from
   # https://en.wikibooks.org/wiki/Algorithm_Implementation/Strings/Longest_common_substring
   def lcs(s1, s2)
     res = ""
@@ -9,9 +9,9 @@ module ZBroker::LCS
     s1.scan(/./).each_with_index do |l1, i|
       s2.scan(/./).each_with_index do |l2, j|
         unless l1 == l2
-          num[i][j]=0
+          num[i][j] = 0
         else
-          (i==0 || j==0) ? num[i][j]=1 : num[i][j]=1 + num[i-1][j-1]
+          (i == 0 || j == 0) ? num[i][j] = 1 : num[i][j] = 1 + num[i-1][j-1]
           if num[i][j] > len
             len = ans = num[i][j]
             thissub = i
