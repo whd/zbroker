3db7a1ab20f37c5cb60b178b7fa8753013084ac8
Wesley Dawson <wdawson@mozilla.com>
2012-08-28 16:28:13 -0700

    Sinatra provides mutex locking (use it) GET requests are with respect to /zbroker

--- lib/zbroker.rb
+++ lib/zbroker.rb
@@ -7,19 +7,15 @@ require "zbroker/zeusbridge"
 
 module ZBroker
   class App < Sinatra::Base
-    config = ZBroker::Config::load_config
-    set :config, config
+    set :run, false
+    set :config, ZBroker::Config::load_config
     set :bridge, ZBroker::ZeusBridge.new(config)
-    set :port, config['port'] if config['port']
-    set :run, true
+    set :port, settings.config['port'] if settings.config['port']
     set :logging, true
-    set :lock, Mutex.new
+    set :lock, true
 
-    before do
-      settings.lock.lock
-    end
-
-    get '/zbroker/api' do
+    # RackBaseURI is set to /zbroker
+    get '/api' do
       # set up a request object
       req = Request.new(request.ip, params)
       # if the request isn't bogus, process it
@@ -28,10 +24,6 @@ module ZBroker
       content_type req.format
       # and serialize it
       req.serialize
-    end
-
-    after do
-      settings.lock.unlock
     end
   end
 end
